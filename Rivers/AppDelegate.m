d86ce778dcdfc8de59e9fcd5c981e0d34d5d1fe0
Antony Kancidrowski <antony.kancidrowski@gmail.com>
2017-09-17 22:12:13 +0100

    Fix debug options persistance

--- Rivers/AppDelegate.m
+++ Rivers/AppDelegate.m
@@ -56,11 +56,17 @@ void exceptionHandler(NSException *exception) {
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    DebugOptions *options = [DebugOptions sharedDebugOptions];
+    [options writeOptions];
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults synchronize];
 }
 
 
@@ -71,6 +77,9 @@ void exceptionHandler(NSException *exception) {
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    DebugOptions *options = [DebugOptions sharedDebugOptions];
+    [options writeOptions];
 }
 
 
